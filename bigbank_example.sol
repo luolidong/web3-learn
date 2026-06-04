3c017d91728d1ecb9efa7d33686f481e9083e785
luolidong <2191193427@qq.com>
2026-06-04 15:57:39 +0800

    add bigbank说明

--- bigbank_example.sol
+++ bigbank_example.sol
@@ -1,4 +1,17 @@
 // SPDX-License-Identifier: MIT
+
+-- 编写 IBank 接口及BigBank 合约，使其满足 Bank 实现 IBank， BigBank 继承自 Bank ， 同时 BigBank 有附加要求：
+
+-- 要求存款金额 >0.001 ether（用modifier权限控制）
+-- BigBank 合约支持转移管理员
+-- 编写一个 Admin 合约， Admin 合约有自己的 Owner ，同时有一个取款函数 adminWithdraw(IBank bank) , adminWithdraw 中会调用 IBank 接口的 withdraw 方法从而把 bank 合约内的资金转移到 Admin 合约地址。
+
+-- BigBank 和 Admin 合约 部署后，把 BigBank 的管理员转移给 Admin 合约地址，模拟几个用户的存款，然后
+
+-- Admin 合约的Owner地址调用 adminWithdraw(IBank bank) 把 BigBank 的资金转移到 Admin 地址。
+
+-- 难点：需调用bigbank的transfer方法，将资金从bigbank合约地址转移到admin合约地址，即使 Admin.owner 和 BigBank.admin 都是同一个用户地址，当 Admin.adminWithdraw() 调用 bank.withdraw() 时， msg.sender 变成了 Admin 合约地址 ，而不是用户地址。
+
 pragma solidity ^0.8.20;
 
 interface IBank {
